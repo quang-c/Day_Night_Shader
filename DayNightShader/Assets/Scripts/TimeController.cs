6cd8d8b0efe58e5f133fd2c57bc70421caa7ca1f
cquang <cindy.quang@hva.nl>
2020-03-18 00:01:50 +0100

    added comments

--- DayNightShader/Assets/Scripts/TimeController.cs
+++ DayNightShader/Assets/Scripts/TimeController.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class TimeController : MonoBehaviour
 {
-
-
-    // 2 minutes is 1 day
-    //[SerializeField] 
-    //private float secondsInFullDay = 120f;
-
-    //[Range(0, 1)]
-    //[SerializeField]
-    //private float currentTimeOfDay = 0f;
-    //private float timeMultiplier = 1f;
-    //private float sunInitialIntensity;
-
-
     [SerializeField] private float degreesPerSec = 0f;
     [SerializeField] private float time;
 
@@ -39,22 +26,8 @@ public class TimeController : MonoBehaviour
     //[SerializeField] private AnimationCurve fogStartDistance;
 
 
-    private void Start()
-    {
-        //sunInitialIntensity = sun.intensity;
-    }
-
     private void Update()
     {
-        // UpdateSun();
-
-        //currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
-
-        //if (currentTimeOfDay >= 1)
-        //{
-        //    // restart day
-        //    currentTimeOfDay = 0f;
-        //}
 
         // begin timer , multiplied by a multiplier
         time += degreesPerSec * Time.deltaTime;
@@ -87,36 +60,6 @@ public class TimeController : MonoBehaviour
         RenderSettings.skybox.SetFloat("_HorizonFogExponent", horizonFog.Evaluate(cycleStage));
         RenderSettings.skybox.SetColor("_SkyTint", skyTint.Evaluate(cycleStage));
         sun.color = sunColor.Evaluate(cycleStage);
+
     }
-
-    //void UpdateSun()
-    //{
-    //    // -90 sunrise, 170 horizon
-    //    sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
-
-    //    float intensityMultiplier = 1f;
-
-    //    // sunrise - sunset
-    //    if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75f)
-    //    {
-    //        //TODO: change values in shader
-    //        intensityMultiplier = 0f;
-    //    }
-    //    // sunrise fade
-    //    else if (currentTimeOfDay <= 0.25f)
-    //    {
-    //        //TODO: change values in shader
-    //        intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
-    //    }
-    //    // sunset fade
-    //    else if (currentTimeOfDay >= 0.73f)
-    //    {
-    //        //TODO: change values in shader
-    //        intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
-    //    }
-
-    //    sun.intensity = sunInitialIntensity * intensityMultiplier;
-
-
-    //}
 }
--- DayNightShader/Assets/SkyboxShader.shader
+++ DayNightShader/Assets/SkyboxShader.shader
@@ -34,12 +34,14 @@
 
 			#define HARDNESS_EXPONENT_BASE 0.125
 
+			// data
             struct appdata
             {
                 float4 vertex : POSITION;
 				float3 texcoord :TEXCOORD0;
             };
 
+			// vertex to fragment struct
 			struct v2f
 			{
 				float4 position : SV_POSITION;
@@ -54,14 +56,16 @@
 			uniform half3 _SkyTint, _MoonColor;
 			uniform half _SunSize, _HorizonFogExponent, _SunHardness, _SunGlareStrength, _MoonSize;
 			
-			half calculateSpot(half3 sunDirPos, half3 skyDirPos, half size, out half distance)
+			// calc sun or moon shape
+			half calculate(half3 sunDirPos, half3 ray, half size, out half distance)
 			{
-				half3 delta = sunDirPos - skyDirPos;
+				half3 delta = sunDirPos - ray;
 				distance = length(delta);
 				half spot = 1.0 - smoothstep(0.0, size, distance);
 				return 1.0 - pow(HARDNESS_EXPONENT_BASE, spot * _SunHardness);
 			}
 
+			// vertex to fragment - vertexshaderoutput
             v2f vert (appdata v)
             {
                 v2f OUT;
@@ -70,20 +74,25 @@
                 return OUT;
             }
 
+			// sample texture - fragmentshaderinput
 			half4 frag(v2f IN) : SV_Target
 			{
+				// uv y
 				half p = IN.texcoord.y;
 
 				half sunDist;
 				half moonDist;
-				half3 sunMie = calculateSpot(_WorldSpaceLightPos0.xyz, IN.texcoord.xyz, _SunSize, sunDist);
-				half3 moonMie = calculateSpot(_MoonPosition.xyz, IN.texcoord.xyz, _MoonSize, moonDist);
+				// sunlight and moonlight scattering
+				half3 sunMie = calculate(_WorldSpaceLightPos0.xyz, IN.texcoord.xyz, _SunSize, sunDist);
+				half3 moonMie = calculate(_MoonPosition.xyz, IN.texcoord.xyz, _MoonSize, moonDist);
 
+				// sun blends with horizon fog when sunrise/sunset
 				float p1 = pow(min(1.0, 1.0 - p), _HorizonFogExponent);
 				float p2 = 1.0 - p1;
 
-				sunDist = 1 - sunDist;
+				sunDist =  1- sunDist;
 
+				
 				half glareMultiplier = saturate((sunDist - _SunGlareStrength) / (1 - _SunGlareStrength));
 
 				half3 col = 0;
